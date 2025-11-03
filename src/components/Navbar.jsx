09924d0a9424f85d5e14e9d8617cda1fe49ea5cd
vishnurdas27 <vishnurdas27@gmail.com>
2025-11-03 20:50:37 +0530

    error solved

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import {Link, useNavigate} from 'react-router-dom'
-import { assets } from '../assets/assets'
 import {MenuIcon, SearchIcon, TicketPlus, XIcon} from 'lucide-react'
 import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
 
